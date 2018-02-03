0995eebbdfcf2f8f8513ce8ebc0c880f8387eb9c
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-02-02 19:51:34 -0800

    changed port to 3001

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('Hello World')
 });
 
 app.listen(PORT, function() {
