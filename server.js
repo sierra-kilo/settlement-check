6164bdde79fc601e07d3008bbbfc4822dbeb0b5b
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-02-03 16:07:20 -0800

    added middleware body-parser to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
 
 app.get('/', function (req, res) {
   res.send('Hello World')
