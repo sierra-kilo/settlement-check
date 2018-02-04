e90a1a2282b54fe86ee6a42afe7699b19c19d5ea
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-02-04 11:42:08 -0800

    yarn lock, changed name of config, to db folder, added seed.sql file

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host    : 'localhost',
   user    : 'root',
   password: '',
-  database: ''
+  database: 'settlement_db'
 });
 
 connection.connect(function(err) {
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+use settlement_db;
+
+INSERT INTO testTable VALUES
+(null,'Orange', 'accident', 'vehicle', 100000);
+INSERT INTO testTable VALUES
+(null,'Lemon', 'accident', 'bike', 100000);
+INSERT INTO testTable VALUES
+(null,'Blueberry', 'accident', 'slip', 100000);
