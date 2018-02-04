e1b91d1e294d25ad167d22728db06c11a4009920
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-02-03 21:14:21 -0800

    revert to original status

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
